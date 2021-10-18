46e6a289d6c5381332b6661253250a1b226bff47
Frank <511095208@qq.com>
2021-10-18 11:51:23 +0800

    add websocket

--- Android/FFTalk/app/src/main/java/com/frank/fftalk/MainActivity.java
+++ Android/FFTalk/app/src/main/java/com/frank/fftalk/MainActivity.java
@@ -3,6 +3,8 @@ package com.frank.fftalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
